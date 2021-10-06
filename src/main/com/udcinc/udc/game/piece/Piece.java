25598f8bbc0057f7da53285e3c032a3780b79fc2
Thomas Presicci <mrbennjerry@gmail.com>
2021-10-06 18:09:02 -0500

    Added getAllValidMoves method

--- src/main/com/udcinc/udc/game/piece/Piece.java
+++ src/main/com/udcinc/udc/game/piece/Piece.java
@@ -1,9 +1,11 @@
 package main.com.udcinc.udc.game.piece;
 
 import javafx.scene.image.Image;
+import main.com.udcinc.udc.game.GameStatics;
 import main.com.udcinc.udc.game.board.Position;
 import main.com.udcinc.udc.game.board.Tile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Piece {
@@ -11,7 +13,7 @@ public abstract class Piece {
     protected String name;
     protected Image image;
 
-    //  GameState vars
+    //  Dynamically assigned vars
     protected boolean isAlive;
     protected Position position;
     protected Tile tile;
@@ -23,12 +25,20 @@ public abstract class Piece {
         this.position = new Position(-1, -1);
     }
 
-    protected boolean canMove(Tile tile) {
+    protected boolean canMove(Tile tile) {  // Tile tile
         return false;
     }
 
     protected List<Tile> getAllValidMoves() {
-        return null;
+        List<Tile> possibleTiles = new ArrayList<>();
+        for (Tile[] row : GameStatics.getGameState().getBoard().getTiles()) {
+            for (Tile tile : row) {
+                if (canMove(tile)) {
+                    possibleTiles.add(tile);
+                }
+            }
+        }
+        return possibleTiles;
     }
 
     public String getName() {
