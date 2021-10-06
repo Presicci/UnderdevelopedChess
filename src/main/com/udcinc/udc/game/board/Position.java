6b047918fa259e17dba77c2fc3624e467615339e
Thomas Presicci <mrbennjerry@gmail.com>
2021-10-06 17:31:04 -0500

    Created Position class for holding tile coordinates

---
+++ src/main/com/udcinc/udc/game/board/Position.java
@@ -0,0 +1,18 @@
+package main.com.udcinc.udc.game.board;
+
+public class Position {
+    private final int x, y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
