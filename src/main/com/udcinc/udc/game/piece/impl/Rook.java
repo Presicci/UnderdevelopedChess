81bb903301bc9436da685f3b8f192ed372086d4d
Thomas Presicci <mrbennjerry@gmail.com>
2021-10-06 18:14:06 -0500

    Added mostly finished rook raycasting

--- src/main/com/udcinc/udc/game/piece/impl/Rook.java
+++ src/main/com/udcinc/udc/game/piece/impl/Rook.java
@@ -1,5 +1,6 @@
 package main.com.udcinc.udc.game.piece.impl;
 
+import main.com.udcinc.udc.game.GameStatics;
 import main.com.udcinc.udc.game.board.Tile;
 import main.com.udcinc.udc.game.piece.Piece;
 
@@ -8,5 +9,50 @@ public class Rook extends Piece {
     	super(tile);
         this.name = "Rook";
         tile.setPiece(this);
+    }
+    
+    @Override
+    public boolean canMove(Tile tile) {
+        boolean canMove =  (tile.getPosition().getX() == getPosition().getX() || tile.getPosition().getY() == getPosition().getY());
+        Tile[][] boardTiles = GameStatics.getGameState().getBoard().getTiles();
+        int tileX = tile.getPosition().getX();
+        int tileY = tile.getPosition().getY();
+        int pieceX = getPosition().getX();
+        int pieceY = getPosition().getY();
+        //if (tile.getPiece().getOwner())   TODO return false if tile contains FRIENDLY piece
+        if (tileX == pieceX) {    // Scan column
+            if (tileY > pieceY) {   // Scan down
+                //  Scan from piece to destination tile for obstructions
+                for (int index = pieceY + 1; index < tileY; ++index) {
+                    if (boardTiles[pieceX][index].hasPiece()) {
+                        return false;
+                    }
+                }
+            } else {    // Scan up
+                //  Scan from piece to destination tile for obstructions
+                for (int index = tileY; index < pieceY; ++index) {
+                    if (index != tileY && boardTiles[pieceX][index].hasPiece()) {
+                        return false;
+                    }
+                }
+            }
+        } else {    // Scan row
+            if (tileX > pieceX) {   // Scan right
+                //  Scan from piece to destination tile for obstructions
+                for (int index = pieceX + 1; index < tileX; ++index) {
+                    if (boardTiles[index][pieceY].hasPiece()) {
+                        return false;
+                    }
+                }
+            } else {    // Scan left
+                //  Scan from piece to destination tile for obstructions
+                for (int index = tileX; index < pieceX; ++index) {
+                    if (index != tileX && boardTiles[index][pieceY].hasPiece()) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return canMove;
     }
 }
