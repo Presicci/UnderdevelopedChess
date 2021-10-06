62ef1554cdbf03c87451ee6577f13cc7b9fc8ee9
Thomas Presicci <mrbennjerry@gmail.com>
2021-10-06 18:10:29 -0500

    Added Rook constructor

--- src/main/com/udcinc/udc/game/piece/impl/Rook.java
+++ src/main/com/udcinc/udc/game/piece/impl/Rook.java
@@ -1,7 +1,14 @@
 package main.com.udcinc.udc.game.piece.impl;
 
+import main.com.udcinc.udc.game.board.Tile;
 import main.com.udcinc.udc.game.piece.Piece;
 
 public class Rook extends Piece {
-
+    public Rook(Tile tile) {
+        this.name = "Rook";
+        this.isAlive = true;
+        this.position = tile.getPosition();
+        this.tile = tile;
+        tile.setPiece(this);
+    }
 }
