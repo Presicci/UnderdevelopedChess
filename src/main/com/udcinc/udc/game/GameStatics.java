056b2992a07ec5dbc33e9987f8aca3625af5fa74
Thomas Presicci <mrbennjerry@gmail.com>
2021-10-06 18:57:20 -0500

    Added init method

--- src/main/com/udcinc/udc/game/GameStatics.java
+++ src/main/com/udcinc/udc/game/GameStatics.java
@@ -3,6 +3,10 @@ package main.com.udcinc.udc.game;
 public class GameStatics {
 	
 	private static GameState gameState;
+	
+	public static void init() {
+		gameState = new GameState();
+	}
 
 	public static GameState getGameState() {
 		return gameState;
