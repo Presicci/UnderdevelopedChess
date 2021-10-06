935f4a4b5e2f8ba3227e9fd5dbbace92b923c2f1
Thomas Presicci <mrbennjerry@gmail.com>
2021-10-06 18:13:19 -0500

    Initial commit

---
+++ src/main/com/udcinc/udc/game/board/Board.java
@@ -0,0 +1,20 @@
+package main.com.udcinc.udc.game.board;
+
+/**
+ * Container which holds a collection of Tiles.
+ */
+public class Board {
+    private Tile[][] tiles;
+
+    public void init(int size) {
+        tiles = new Tile[size][size];
+    }
+
+    public void assignTile(Tile tile, int x, int y) {
+        tiles[x][y] = tile;
+    }
+
+    public Tile[][] getTiles() {
+        return tiles;
+    }
+}
