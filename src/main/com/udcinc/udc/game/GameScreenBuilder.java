97b61025a207098cbd007457e5058ddf17cd96f0
Thomas Presicci <mrbennjerry@gmail.com>
2021-10-06 17:23:13 -0500

    Initial commit

---
+++ src/main/com/udcinc/udc/game/GameScreenBuilder.java
@@ -0,0 +1,14 @@
+package main.com.udcinc.udc.game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameScreenBuilder extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
