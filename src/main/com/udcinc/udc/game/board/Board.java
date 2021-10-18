deddff9053f3ee080fca3c199c32863157da3ced
Thomas Presicci <thomaspresicci@gmail.com>
2021-10-18 14:35:05 -0500

    Fixed killPiece() method ordering, yikes

--- src/main/com/udcinc/udc/game/board/Board.java
+++ src/main/com/udcinc/udc/game/board/Board.java
@@ -33,8 +33,8 @@ public class Board {
 	}
 	
 	public void killPiece(Piece piece) {
-		piece.doKill();
 		tiles[piece.getPosition().getX()][piece.getPosition().getY()].setPiece(null);
+		piece.doKill();
 	}
 	
 	public void movePiece(Piece piece, Position nextPos) {
