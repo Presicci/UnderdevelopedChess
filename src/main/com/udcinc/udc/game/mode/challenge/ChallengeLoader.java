7d43e77fb0e17140c10fb9d85fcfbe9d5cb1a9d5
Thomas Presicci <thomaspresicci@gmail.com>
2021-10-18 14:42:26 -0500

    Initial commit

---
+++ src/main/com/udcinc/udc/game/mode/challenge/ChallengeLoader.java
@@ -0,0 +1,5 @@
+package main.com.udcinc.udc.game.mode.challenge;
+
+public class ChallengeLoader {
+
+}
---
+++ src/main/com/udcinc/udc/game/mode/challenge/ChallengeRules.java
@@ -0,0 +1,7 @@
+package main.com.udcinc.udc.game.mode.challenge;
+
+import main.com.udcinc.udc.game.GameRules;
+
+public class ChallengeRules extends GameRules {
+
+}
