a62806cffe932e57869346195c632e61d9593115
Thomas Presicci <thomaspresicci@gmail.com>
2021-10-20 14:43:08 -0500

    Added numberOfturns and timerLimit defaults to game rules

--- src/main/com/udcinc/udc/game/GameRules.java
+++ src/main/com/udcinc/udc/game/GameRules.java
@@ -1,5 +1,42 @@
 package main.com.udcinc.udc.game;
 
+/**
+ * Default game rules object
+ * Contains all data relating to how the game will work
+ * 
+ * @author Thomas Presicci
+ */
 public class GameRules {
+	/*
+	 * Number of turns a player can take in succession
+	 * 
+	 * defaults to 1
+	 */
+	private int numberOfTurns = 1;
+	
+	/*
+	 * Time a player has to take their turns
+	 * TODO maybe implement the full chess timing rules idk yet
+	 * set to 0 to disable timers
+	 * 
+	 * defaults to 0
+	 */
+	private int timerLimit = 0;
+	
+	// Getters/setters
+	public int getTimerLimit() {
+		return timerLimit;
+	}
 
+	public void setTimerLimit(int timerLimit) {
+		this.timerLimit = timerLimit;
+	}
+
+	public int getNumberOfTurns() {
+		return numberOfTurns;
+	}
+
+	public void setNumberOfTurns(int numberOfTurns) {
+		this.numberOfTurns = numberOfTurns;
+	}
 }
