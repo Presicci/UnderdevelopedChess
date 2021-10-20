b67f5b2734041da330cafb35658c493101d6215c
Thomas Presicci <thomaspresicci@gmail.com>
2021-10-20 14:07:58 -0500

    Migrated most of GameSceneBuilder's start method to the initialize mothod of GameSceneController

--- src/main/com/udcinc/udc/game/scene/GameSceneBuilder.java
+++ src/main/com/udcinc/udc/game/scene/GameSceneBuilder.java
@@ -2,23 +2,9 @@ package main.com.udcinc.udc.game.scene;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import main.com.udcinc.udc.game.GameSettings;
-import main.com.udcinc.udc.game.board.Position;
-import main.com.udcinc.udc.game.board.Tile;
-import main.com.udcinc.udc.game.piece.Piece;
-import main.com.udcinc.udc.game.piece.impl.Bishop;
-import main.com.udcinc.udc.game.piece.impl.Queen;
-import main.com.udcinc.udc.game.piece.impl.Rook;
-import main.com.udcinc.udc.game.player.Player;
 import main.com.udcinc.udc.game.state.GameState;
 
 /**
@@ -28,194 +14,25 @@ import main.com.udcinc.udc.game.state.GameState;
  * 
  * @author Thomas Presicci
  */
-public class GameSceneBuilder extends Application {
-	
-	private GridPane boardPane;
-	private static Piece selectedPiece;
+public class GameSceneBuilder extends Application {	
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-        //  Active game scene
-		boardPane = FXMLLoader.load(getClass().getResource("gameScreen.fxml"));
+        // Active game scene
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("gameScreen.fxml"));
 		
-		GameSceneController controller = new GameSceneController(boardPane);
+		// Initialize our game state
+		GameState gs = new GameState();
 		
-		//  Initialize our game state
-		GameState gs = new GameState(controller);
-
-        //  Handles hiding/showing GridPane columns/rows based on the desired dimensions, defined in Settings.java
-        final double tileSize = 800.0 / GameSettings.getSize();
-        for (int index = 0; index < boardPane.getRowConstraints().size(); index++) {
-            if (index > GameSettings.getSize() - 1) {
-            	boardPane.getRowConstraints().get(index).setMaxHeight(0);
-            	boardPane.getColumnConstraints().get(index).setMaxWidth(0);
-            } else {
-            	boardPane.getRowConstraints().get(index).setMaxHeight(tileSize);
-            	boardPane.getColumnConstraints().get(index).setMaxWidth(tileSize);
-            }
-        }
-
-        //  Create the board
-        for (int row = 0; row < GameSettings.getSize(); row++) {
-            for (int column = 0; column < GameSettings.getSize(); column++) {
-                StackPane tile = new StackPane();
-                
-                // Generates the tile pattern
-                String tileColor;
-                if ((row + column) % 2 == 0) {
-                    tileColor = "white";
-                } else {
-                    tileColor = "black";
-                }
-                tile.setStyle("-fx-background-color: " + tileColor);
-                
-                // ImageView to be used for pieces
-                ImageView iv = new ImageView();
-                // Sets dimensions to 80x80
-                iv.setFitHeight(80);
-                iv.setFitWidth(80);
-                // So it doesn't display yet
-                iv.setOpacity(0);
-                
-                // Finals used for event registration lambdas
-                int finalRow = row;
-                int finalColumn = column;
-                
-                //	Handles clicking on a piece
-                iv.setOnMouseClicked(event -> {
-                	// Reset the board to its default coloring before doing move coloring
-                	controller.resetBoardColors();
-                	
-                    Tile boardTile = gs.getBoard().getTiles()[finalRow][finalColumn];
-                    Piece piece = boardTile.getPiece();
-                    
-                    // Debug information
-                    System.out.println("[" + (piece == null ? "" : piece.getName()) + "] owned by " + (piece == null ? "" : piece.getOwner().getName()));
-                    System.out.println("Clicked: " + finalRow + ", " + finalColumn);
-                    
-                    // Recolors the board based on which tiles the piece can move to
-                    if (piece != null) {
-                        for (Tile t : piece.getAllValidMoves()) {
-                            for (Node node : boardPane.getChildren()) {
-                                if (GridPane.getRowIndex(node) == t.getPosition().getY()
-                                        && GridPane.getColumnIndex(node) == t.getPosition().getX()) {
-                                    node.setStyle("-fx-background-color: orange");	// temp color assignments for testing
-                                }
-                            }
-                        }
-                    }
-                });
-                
-                // Start drag and drop
-                iv.setOnDragDetected(event -> {
-                	Piece piece = gs.getBoard().getTiles()[finalRow][finalColumn].getPiece();
-                	if (piece == null) {
-                		return;
-                	}
-                	
-                	// Dragboard carries data with the mouse
-                	Dragboard db = iv.startDragAndDrop(TransferMode.ANY);
-                	// ClipboardContent carries the image
-                	ClipboardContent content = new ClipboardContent();
-                	content.putImage(iv.getImage());
-                	// Set the content to the dragboard
-                	db.setContent(content);
-                	
-                	// Sets the piece being dragged
-                	selectedPiece = piece;
-                	
-                	for (Tile t : piece.getAllValidMoves()) {
-                        for (Node node : boardPane.getChildren()) {
-                            if (GridPane.getRowIndex(node) == t.getPosition().getY()
-                                    && GridPane.getColumnIndex(node) == t.getPosition().getX()) {
-                                node.setStyle("-fx-background-color: orange");	// temp color assignments for testing
-                            }
-                        }
-                    }
-                	
-                	event.consume();
-                });
-                
-                // Used in the case of a drag not ending on a tile
-                iv.setOnDragDone(event -> {
-                	selectedPiece = null;
-                });
-                
-                // Handles transfer mode acceptance of the piece on the tile
-                iv.setOnDragOver(event -> {
-                	if (event.getGestureSource() != iv && event.getDragboard().hasImage()) {
-                		event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-                	}
-                	event.consume();
-                });
-                
-                // Handles transfer mode acceptance of the tile
-                tile.setOnDragOver(event -> {
-                	if (event.getGestureSource() != iv && event.getDragboard().hasImage()) {
-                		event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-                	}
-                	event.consume();
-                });
-                
-                // When a piece is dropped onto another piece
-                iv.setOnDragDropped(event -> {
-                	Tile boardTile = gs.getBoard().getTiles()[finalRow][finalColumn];
-                	// If selected piece is not null, move the piece
-                	if (selectedPiece != null) {
-                		if (selectedPiece.canMove(boardTile)) {
-                			if (boardTile.getPiece() != null) {
-                				gs.getBoard().killPiece(boardTile.getPiece());
-                			}
-                			gs.getBoard().movePiece(selectedPiece, new Position(finalRow, finalColumn));
-                    	} else {
-                    		System.out.println("Invalid move!");
-                    	}
-                	}
-                	
-                	controller.resetBoardColors();
-                });
-                
-                // When a piece is dropped onto a tile
-                // *tile may or may not contain another piece
-                tile.setOnDragDropped(event -> {                	
-                	Tile boardTile = gs.getBoard().getTiles()[finalRow][finalColumn];
-                	// If selected piece is not null, move the piece
-                	if (selectedPiece != null) {
-                		if (selectedPiece.canMove(boardTile)) {
-                			if (boardTile.getPiece() != null) {
-                				gs.getBoard().killPiece(boardTile.getPiece());
-                			}
-                			gs.getBoard().movePiece(selectedPiece, new Position(finalRow, finalColumn));
-                    	} else {
-                    		System.out.println("Invalid move!");
-                    	}
-                	}
-                	
-                	controller.resetBoardColors();
-                });
-                
-                // Add the tile to the gridpane
-                boardPane.add(tile, row, column);
-                boardPane.add(iv, row, column);
-
-                //  Game manager assignments
-                Tile boardTile = new Tile(column, row);
-                gs.getBoard().assignTile(boardTile, column, row);
-            }
-        }
-
-        // Test player
-        Player player = new Player("TestPlayer");
-        Player player2 = new Player("TestPlayer2");
-        
-        //  Test objects
-        gs.getBoard().addPiece(new Rook(player, gs.getBoard().getTiles()[1][1], gs));
-        gs.getBoard().addPiece(new Bishop(player, gs.getBoard().getTiles()[1][4], gs));
-        gs.getBoard().addPiece(new Queen(player2, gs.getBoard().getTiles()[3][2], gs));
+		// Overloads the default controller constructor
+		loader.setControllerFactory(GameSceneController -> new GameSceneController(gs));
+		
+		// Gets root pane for the scene
+		Pane root = loader.load();
 
         // Setup the window
-        primaryStage.setTitle("test");
-        primaryStage.setScene(new Scene(boardPane, 800, 800));
+        primaryStage.setTitle("Underdeveloped Chess");
+        primaryStage.setScene(new Scene(root, 800, 800));
         primaryStage.show();
 	}
 	
--- src/main/com/udcinc/udc/game/scene/GameSceneController.java
+++ src/main/com/udcinc/udc/game/scene/GameSceneController.java
@@ -1,19 +1,71 @@
 package main.com.udcinc.udc.game.scene;
 
+import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.effect.Light;
 import javafx.scene.effect.Lighting;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import main.com.udcinc.udc.game.GameSettings;
+import main.com.udcinc.udc.game.board.Position;
+import main.com.udcinc.udc.game.board.Tile;
 import main.com.udcinc.udc.game.piece.Piece;
+import main.com.udcinc.udc.game.piece.impl.Bishop;
+import main.com.udcinc.udc.game.piece.impl.Queen;
+import main.com.udcinc.udc.game.piece.impl.Rook;
+import main.com.udcinc.udc.game.player.Player;
+import main.com.udcinc.udc.game.state.GameState;
 
 public class GameSceneController {
 
-	private GridPane boardPane;
+	// Reference to the gridPane that is used for the board
+	@FXML private GridPane board;
 	
-	public GameSceneController(GridPane boardPane) {
-		this.boardPane = boardPane;
+	private GameState gs;
+	
+	// static piece object used for storing object data during drag and drop
+	private static Piece selectedPiece;
+	
+	/**
+	 * This constructor overrides the base FXML constructor via
+	 * a constructor factory assignment in GameSceneBuilder.java
+	 * @param gs The gamestate model
+	 */
+	public GameSceneController(GameState gs) {
+		this.gs = gs;
+	}
+	
+	/**
+	 * Initialize is called after the constructor
+	 */
+	@FXML
+	private void initialize() {
+		setupGrid();
+
+        //  Create the board
+        for (int row = 0; row < GameSettings.getSize(); row++) {
+            for (int column = 0; column < GameSettings.getSize(); column++) {
+                registerTile(row,  column);
+            }
+        }
+        
+        /*
+         * Test data
+         */
+        
+        // Test player
+        Player player = new Player("TestPlayer");
+        Player player2 = new Player("TestPlayer2");
+        
+        //  Test objects
+        assignPieceToBoard(new Rook(player, gs.getBoard().getTiles()[1][1], gs));
+        assignPieceToBoard(new Bishop(player, gs.getBoard().getTiles()[1][4], gs));
+        assignPieceToBoard(new Queen(player2, gs.getBoard().getTiles()[3][2], gs));
 	}
 	
 	/**
@@ -22,7 +74,7 @@ public class GameSceneController {
 	 * @param root The GridPane containing the board tiles.
 	 */
 	public void resetBoardColors() {
-		for (Node node : boardPane.getChildren()) {
+		for (Node node : board.getChildren()) {
 			if ((GridPane.getRowIndex(node) + GridPane.getColumnIndex(node)) % 2 == 0) {
 				node.setStyle("-fx-background-color: white");
 			} else {
@@ -39,7 +91,7 @@ public class GameSceneController {
 	 * @param piece The piece being added to the board
 	 */
 	public void assignPieceToBoard(Piece piece) {
-		for (Node node : boardPane.getChildren()) {
+		for (Node node : board.getChildren()) {
 			if (node instanceof ImageView) {
 				if (GridPane.getRowIndex(node) == piece.getPosition().getY() 
 						&& GridPane.getColumnIndex(node) == piece.getPosition().getX()) {
@@ -67,7 +119,7 @@ public class GameSceneController {
 	 * @param piece
 	 */
 	public void removePieceFromBoard(Piece piece) {
-		for (Node node : boardPane.getChildren()) {
+		for (Node node : board.getChildren()) {
 			if (node instanceof ImageView) {
 				if (GridPane.getRowIndex(node) == piece.getPosition().getY() 
 						&& GridPane.getColumnIndex(node) == piece.getPosition().getX()) {
@@ -76,5 +128,182 @@ public class GameSceneController {
 				}
 			}
 		}
+	}
+	
+	public void setupGrid() {
+		final double tileSize = 600.0 / GameSettings.getSize();
+        for (int index = 0; index < board.getRowConstraints().size(); index++) {
+            if (index > GameSettings.getSize() - 1) {
+            	board.getRowConstraints().get(index).setMaxHeight(0);
+            	board.getColumnConstraints().get(index).setMaxWidth(0);
+            } else {
+            	board.getRowConstraints().get(index).setMaxHeight(tileSize);
+            	board.getColumnConstraints().get(index).setMaxWidth(tileSize);
+            }
+        }
+	}
+	
+	/**
+	 * Sets event handling and colors the tile at index[row][column]
+	 * and assigns this tile to the board
+	 * @param row The row index
+	 * @param column The column index
+	 */
+	public void registerTile(final int row, final int column) {
+		StackPane tile = new StackPane();
+        
+        // Generates the tile pattern
+        String tileColor;
+        if ((row + column) % 2 == 0) {
+            tileColor = "white";
+        } else {
+            tileColor = "black";
+        }
+        tile.setStyle("-fx-background-color: " + tileColor);
+        
+        // ImageView to be used for pieces
+        ImageView iv = new ImageView();
+        // Sets dimensions to 80x80
+        iv.setFitHeight(80);
+        iv.setFitWidth(80);
+        // So it doesn't display yet
+        iv.setOpacity(0);
+        
+        //	Handles clicking on a piece
+        iv.setOnMouseClicked(event -> {
+        	// Reset the board to its default coloring before doing move coloring
+        	resetBoardColors();
+        	
+            Tile boardTile = gs.getBoard().getTiles()[row][column];
+            Piece piece = boardTile.getPiece();
+            
+            // Debug information
+            System.out.println("[" + (piece == null ? "" : piece.getName()) + "] owned by " + (piece == null ? "" : piece.getOwner().getName()));
+            System.out.println("Clicked: " + row + ", " + column);
+            
+            // Recolors the board based on which tiles the piece can move to
+            if (piece != null) {
+                for (Tile t : piece.getAllValidMoves()) {
+                    for (Node node : board.getChildren()) {
+                        if (GridPane.getRowIndex(node) == t.getPosition().getY()
+                                && GridPane.getColumnIndex(node) == t.getPosition().getX()) {
+                            node.setStyle("-fx-background-color: orange");	// temp color assignments for testing
+                        }
+                    }
+                }
+            }
+        });
+        
+        // Start drag and drop
+        iv.setOnDragDetected(event -> {
+        	Piece piece = gs.getBoard().getTiles()[row][column].getPiece();
+        	if (piece == null) {
+        		return;
+        	}
+        	
+        	// Dragboard carries data with the mouse
+        	Dragboard db = iv.startDragAndDrop(TransferMode.ANY);
+        	// ClipboardContent carries the image
+        	ClipboardContent content = new ClipboardContent();
+        	content.putImage(iv.getImage());
+        	// Set the content to the dragboard
+        	db.setContent(content);
+        	
+        	// Sets the piece being dragged
+        	selectedPiece = piece;
+        	
+        	for (Tile t : piece.getAllValidMoves()) {
+                for (Node node : board.getChildren()) {
+                    if (GridPane.getRowIndex(node) == t.getPosition().getY()
+                            && GridPane.getColumnIndex(node) == t.getPosition().getX()) {
+                        node.setStyle("-fx-background-color: orange");	// temp color assignments for testing
+                    }
+                }
+            }
+        	
+        	event.consume();
+        });
+        
+        // Used in the case of a drag not ending on a tile
+        iv.setOnDragDone(event -> {
+        	selectedPiece = null;
+        });
+        
+        // Handles transfer mode acceptance of the piece on the tile
+        iv.setOnDragOver(event -> {
+        	if (event.getGestureSource() != iv && event.getDragboard().hasImage()) {
+        		event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        	}
+        	event.consume();
+        });
+        
+        // Handles transfer mode acceptance of the tile
+        tile.setOnDragOver(event -> {
+        	if (event.getGestureSource() != iv && event.getDragboard().hasImage()) {
+        		event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+        	}
+        	event.consume();
+        });
+        
+        // When a piece is dropped onto another piece
+        iv.setOnDragDropped(event -> {
+        	Tile boardTile = gs.getBoard().getTiles()[row][column];
+        	// If selected piece is not null, move the piece
+        	if (selectedPiece != null) {
+        		if (selectedPiece.canMove(boardTile)) {
+        			if (boardTile.getPiece() != null) {
+        				gs.getBoard().killPiece(boardTile.getPiece());
+        			}
+        			movePiece(selectedPiece, new Position(row, column));
+            	} else {
+            		System.out.println("Invalid move!");
+            	}
+        	}
+        	
+        	resetBoardColors();
+        });
+        
+        // When a piece is dropped onto a tile
+        // *tile may or may not contain another piece
+        tile.setOnDragDropped(event -> {                	
+        	Tile boardTile = gs.getBoard().getTiles()[row][column];
+        	// If selected piece is not null, move the piece
+        	if (selectedPiece != null) {
+        		if (selectedPiece.canMove(boardTile)) {
+        			if (boardTile.getPiece() != null) {
+        				gs.getBoard().killPiece(boardTile.getPiece());
+        			}
+        			movePiece(selectedPiece, new Position(row, column));
+            	} else {
+            		System.out.println("Invalid move!");
+            	}
+        	}
+        	
+        	resetBoardColors();
+        });
+        
+        // Add the tile to the gridpane
+        board.add(tile, row, column);
+        board.add(iv, row, column);
+
+        //  Game manager assignments
+        Tile boardTile = new Tile(column, row);
+        gs.getBoard().assignTile(boardTile, column, row);
+	}
+	
+	/**
+	 * Moves a piece to a new tile position
+	 * @param piece The piece to move
+	 * @param nextPos The position to move the piece to
+	 */
+	private void movePiece(Piece piece, Position nextPos) {		
+		// Visually remove the piece from old tile
+		removePieceFromBoard(piece);
+		
+		// Move the piece to the new tile
+		piece.move(nextPos);
+		
+		// Visually add the piece to new tile
+		assignPieceToBoard(piece);
 	}
 }
