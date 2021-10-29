2c00117756a9893fcf4b3fcf39389f9dadf0f341
Thomas Presicci <thomaspresicci@gmail.com>
2021-10-29 13:52:15 -0500

    Implemented knight moveset with canMove method

--- src/main/com/udcinc/udc/game/piece/impl/Knight.java
+++ src/main/com/udcinc/udc/game/piece/impl/Knight.java
@@ -1,5 +1,7 @@
 package main.com.udcinc.udc.game.piece.impl;
 
+import javafx.scene.image.Image;
+import main.com.udcinc.udc.game.board.Position;
 import main.com.udcinc.udc.game.board.Tile;
 import main.com.udcinc.udc.game.piece.Piece;
 import main.com.udcinc.udc.game.player.Player;
@@ -10,6 +12,24 @@ public class Knight extends Piece {
 	public Knight(Player player, Tile tile, GameState gs) {
     	super(player, tile, gs);
 		this.name = "Knight";
+        this.image = new Image("./rook.png");	// temp image location
 	}
 
+
+	@Override
+	public boolean canMove(Tile tile) {
+		Position pPos = this.getPosition();
+		Position tPos = tile.getPosition();
+		
+		if (pPos.getX() == tPos.getX() + 1 || pPos.getX() == tPos.getX() - 1) {
+			if (Math.abs(pPos.getY() - tPos.getY()) == 2 && !(tile.hasPiece() && tile.getPiece().getOwner() == this.getOwner())) {
+				return true;
+			}
+		} else if (pPos.getX() == tPos.getX() + 2 || pPos.getX() == tPos.getX() - 2) {
+			if (Math.abs(pPos.getY() - tPos.getY()) == 1 && !(tile.hasPiece() && tile.getPiece().getOwner() == this.getOwner())) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
