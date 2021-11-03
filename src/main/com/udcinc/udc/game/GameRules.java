4a07ac26c4308d913bb0117ed80d5824f9216eeb
Thomas Presicci <thomaspresicci@gmail.com>
2021-11-03 13:17:32 -0500

    Added settings booleans for the different 'special' moves

--- src/main/com/udcinc/udc/game/GameRules.java
+++ src/main/com/udcinc/udc/game/GameRules.java
@@ -7,6 +7,25 @@ package main.com.udcinc.udc.game;
  * @author Thomas Presicci
  */
 public class GameRules {
+	/**
+	 * Castling notes
+	 * King cant move over a space that would put it in check
+	 * All spaces between king and rook must be open
+	 * King and rook must not have moved
+	 */
+	
+	/**
+	 * Stalemate
+	 * If player cant move, but isnt in check game ends in a draw	
+	 * If only two kings remain, its a draw
+	 * If one player has only a king, other has only a king and bishop/knight, its a draw
+	 * If exact same positions repeat 3 times, its a draw
+	 * If for 50 moves no pieces are captured and no pawns move, its a draw
+	 * Can offer a draw
+	 */
+	
+	
+	
 	/*
 	 * Number of turns a player can take in succession
 	 * 
@@ -22,6 +41,34 @@ public class GameRules {
 	 * defaults to 0
 	 */
 	private int timerLimit = 0;
+	
+	/*
+	 * Whether castling is allowed or not
+	 * set to false to disable castling
+	 * 
+	 * defaults to true
+	 */
+	private boolean castling = true;
+	
+	/*
+	 * Whether pawns promote when they reach the other end of the board
+	 * set to false to disable promotions
+	 * 
+	 * defaults to true
+	 */
+	private boolean pawnPromotion = true;
+	
+	/*
+	 * Whether pawns can capture horizontally adjacent pawns in the center of the board
+	 * set to false to disable en passant
+	 * 
+	 * defaults to true
+	 */
+	private boolean enPassant = true;
+	
+	
+	
+	
 	
 	// Getters/setters
 	public int getTimerLimit() {
@@ -39,4 +86,30 @@ public class GameRules {
 	public void setNumberOfTurns(int numberOfTurns) {
 		this.numberOfTurns = numberOfTurns;
 	}
+
+	public boolean isCastling() {
+		return castling;
+	}
+
+	public void setCastling(boolean castling) {
+		this.castling = castling;
+	}
+
+	public boolean isPawnPromotion() {
+		return pawnPromotion;
+	}
+
+	public void setPawnPromotion(boolean pawnPromotion) {
+		this.pawnPromotion = pawnPromotion;
+	}
+
+	public boolean isEnPassant() {
+		return enPassant;
+	}
+
+	public void setEnPassant(boolean enPassant) {
+		this.enPassant = enPassant;
+	}
+	
+	
 }
