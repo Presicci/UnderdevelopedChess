710433e50029780b5b60eaf7e8cb8a151592bbd2
Thomas Presicci <thomaspresicci@gmail.com>
2021-11-15 12:11:27 -0600

    Comments

--- src/main/com/udcinc/udc/game/GameSettings.java
+++ src/main/com/udcinc/udc/game/GameSettings.java
@@ -8,9 +8,21 @@ package main.com.udcinc.udc.game;
  * @author Thomas Presicci
  */
 public class GameSettings {
+	/*
+	 * Board is created and scaled based on the size
+	 * defaults to 8
+	 */
     private int size = 8;
+    
+    /*
+     * If possible moves for a piece should be displayed to the player
+     * defaults to true 
+     */
     private boolean enableMoveHighlighting = true;
     
+    
+    
+    // Getters/setters
     public int getSize() {
         return size;
     }
